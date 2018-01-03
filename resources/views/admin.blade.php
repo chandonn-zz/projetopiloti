5c5b4f4f4f77a492ad3827075276b105680ffa9b
Chandonn <alexandrecorreadeoliveira@gmail.com>
2018-01-03 12:43:05 -0200

    Created new routes and a view for the Admin provider

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Painel de Usuário</div>
+                <div class="panel-heading">Painel de Administrador</div>
 
                 <div class="panel-body">
                     @if (session('status'))

863ba093d1c26276878b7b95e4f1f72028d2e22d
Chandonn <alexandrecorreadeoliveira@gmail.com>
2018-01-03 20:51:45 -0200

    Changed guard checking settings to properly validade redirection, users and admins

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,20 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        // verifica se o usuário tenta acessar uma'página enquanto ainda está logado
+        // e o redireciona para o dashboard correto, dependendo do seu 'guard'
+        switch ($guard) {
+            case 'admin':
+                if (Auth::guard($guard)->check()) {
+                    return redirect()->route('admin.dashboard');
+                }
+                break;
+            
+            default:
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/home');
+                }
+                break;
         }
 
         return $next($request);
